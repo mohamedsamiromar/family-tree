a1c724403fa9844e859a88e79c50fda8f39398be
mohamed samir <56839321+mohamedsamiromar@users.noreply.github.com>
2022-08-31 20:12:36 +0200

    install djoser and jwt

--- family_tree/settings.py
+++ family_tree/settings.py
@@ -38,7 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'customuser',
-    'family'
+    'family',
+
+    'rest_framework',
+    'djoser',
+
 ]
 
 MIDDLEWARE = [
